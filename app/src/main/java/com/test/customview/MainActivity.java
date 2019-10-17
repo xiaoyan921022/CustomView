b0c995ca7cbb4943acd3f8b5cc36cebf290944db
yan <yanquanzhou@ichangtou.com>
2019-10-17 17:57:33 +0800

    [fix]增加贝塞尔曲线三阶曲线示例

--- app/src/main/java/com/test/customview/MainActivity.java
+++ app/src/main/java/com/test/customview/MainActivity.java
@@ -7,10 +7,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btn_canvas,btn_canvas2, btn_bezier1, btn_bezier2;
+    private Button btn_canvas,btn_canvas2, btn_bezier1, btn_bezier2,btn_bezier3;
     private CustomView canvas;
     private CustomView2 canvas2;
     private Bezier bezier;
+    private Bezier2 bezier2;
     private MagicCircle circle;
 
     @Override
@@ -21,15 +22,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_canvas2 = findViewById(R.id.btn_canvas2);
         btn_bezier1 = findViewById(R.id.btn_bezier1);
         btn_bezier2 = findViewById(R.id.btn_bezier2);
+        btn_bezier3 = findViewById(R.id.btn_bezier3);
 
         canvas = findViewById(R.id.canvas);
         canvas2 = findViewById(R.id.canvas2);
         bezier = findViewById(R.id.bezier);
+        bezier2 = findViewById(R.id.bezier2);
         circle = findViewById(R.id.circle);
         btn_canvas.setOnClickListener(this);
         btn_canvas2.setOnClickListener(this);
         btn_bezier1.setOnClickListener(this);
         btn_bezier2.setOnClickListener(this);
+        btn_bezier3.setOnClickListener(this);
     }
 
     @Override
@@ -39,24 +43,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 setViewVisible(canvas,true);
                 setViewVisible(canvas2,false);
                 setViewVisible(bezier,false);
+                setViewVisible(bezier2,false);
                 setViewVisible(circle,false);
                 break;
             case R.id.btn_canvas2:
                 setViewVisible(canvas,false);
                 setViewVisible(canvas2,true);
                 setViewVisible(bezier,false);
+                setViewVisible(bezier2,false);
                 setViewVisible(circle,false);
                 break;
             case R.id.btn_bezier1:
                 setViewVisible(canvas,false);
                 setViewVisible(canvas2,false);
                 setViewVisible(bezier,true);
+                setViewVisible(bezier2,false);
                 setViewVisible(circle,false);
                 break;
             case R.id.btn_bezier2:
                 setViewVisible(canvas,false);
                 setViewVisible(canvas2,false);
                 setViewVisible(bezier,false);
+                setViewVisible(bezier2,true);
+                setViewVisible(circle,false);
+                break;
+            case R.id.btn_bezier3:
+                setViewVisible(canvas,false);
+                setViewVisible(canvas2,false);
+                setViewVisible(bezier,false);
+                setViewVisible(bezier2,false);
                 setViewVisible(circle,true);
                 circle.startAnimation();
                 break;
